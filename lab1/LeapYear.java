ba9e8aedd328a13b8a0cc3cdc8040d26209039d4
wusai2333 <wusai2333@hotmail.com>
2017-07-12 15:17:57 -0700

    LeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,11 +1,18 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Sai Wu
  */
 public class LeapYear {
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
+    public static boolean isLeapYear(int year) {
+        if (year%4 == 0) {
+             return true;
+        }
+        return false;
+    }
+    
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
